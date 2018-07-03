8ddff8f8c7dc1ce456303851fcafb1b544f95d5a
MinhTam <minhtamho91@gmail.com>
2018-07-03 10:57:39 +0700

    add quản lý report pdf

--- src/pages/PDFReport/index.js
+++ src/pages/PDFReport/index.js
@@ -6,26 +6,30 @@ import { Tabs } from 'antd'
 const { TabPane } = Tabs
 
 class PDFReportPage extends Component {
-    static defaultProps = {
-        pathName: 'PDF Report Management',
-    }
+  static defaultProps = {
+    pathName: 'PDF Report Management',
+  }
 
-    render() {
-        const props = { ...this.props }
-        return (
-            <Page {...props}>
-                <Helmet title="PDF Report Management" />
-                <section className="card">
-                    <div className="card-body">
-                        <Tabs defaultActiveKey="1" size='small'>
-                            <TabPane tab="Upload PDF" key="1">Form upload</TabPane>
-                            <TabPane tab="Category Management" key="2">Form category</TabPane>
-                        </Tabs>
-                    </div>
-                </section>
-            </Page>
-        )
-    }
+  render() {
+    const props = { ...this.props }
+    return (
+      <Page {...props}>
+        <Helmet title="PDF Report Management" />
+        <section className="card">
+          <div className="card-body">
+            <Tabs defaultActiveKey="1" size="small">
+              <TabPane tab="Upload PDF" key="1">
+                Form upload
+              </TabPane>
+              <TabPane tab="Category Management" key="2">
+                Form category
+              </TabPane>
+            </Tabs>
+          </div>
+        </section>
+      </Page>
+    )
+  }
 }
 
 export default PDFReportPage
