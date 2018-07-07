a1c28a64ff7102527dce926986916282da8b1a8a
vuonglb <lambaovuong_12a1@yahoo.com>
2018-07-07 16:10:01 +0700

    init view for test fabric

--- src/pages/PreProduction/TestFabric/relax/index.js
+++ src/pages/PreProduction/TestFabric/relax/index.js
@@ -1,43 +1,48 @@
 import React, { Component } from 'react'
 import { Table } from 'antd'
 
-
 class TestFabricRelax extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data_detail: []
+      data_detail: [],
     }
   }
-  componentWillReceiveProps = (nextProps) => {
+  componentWillReceiveProps = nextProps => {
     //console.log('TestFabricRelax componentWillReceiveProps =>' + JSON.stringify(nextProps))
-    this.setState({ data_detail: nextProps.data });
+    this.setState({ data_detail: nextProps.data })
   }
   render() {
     const columns = [
-      { key: 'fabric_type', dataIndex: 'fabric_tye', title: 'TYPE', name: 'TYPE', },
-      { key: 'fabric_color', dataIndex: 'fabric_color', title: 'COLOR', name: 'COLOR', },
-      { key: 'roll', dataIndex: 'roll', title: 'ROLL', name: 'ROLL', },
-      { key: 'met', dataIndex: 'met', title: 'MET', name: 'MET', },
-      { key: 'relax', dataIndex: 'relax', title: 'RELAX', name: 'RELAX', },
-      { key: 'condition_hours', dataIndex: 'condition_hours', title: 'CONDITIONS(H)', name: 'CONDITIONS(H)', },
-      { key: 'note', dataIndex: 'note', title: 'NOTE', name: 'NOTE', },
-      { key: 'start_date', dataIndex: 'start_date', title: 'START DATE', name: 'START DATE', },
-      { key: 'end_date', dataIndex: 'end_date', title: 'END DATE', name: 'END DATE', },
-
+      { key: 'fabric_type', dataIndex: 'fabric_tye', title: 'TYPE', name: 'TYPE' },
+      { key: 'fabric_color', dataIndex: 'fabric_color', title: 'COLOR', name: 'COLOR' },
+      { key: 'roll', dataIndex: 'roll', title: 'ROLL', name: 'ROLL' },
+      { key: 'met', dataIndex: 'met', title: 'MET', name: 'MET' },
+      { key: 'relax', dataIndex: 'relax', title: 'RELAX', name: 'RELAX' },
+      {
+        key: 'condition_hours',
+        dataIndex: 'condition_hours',
+        title: 'CONDITIONS(H)',
+        name: 'CONDITIONS(H)',
+      },
+      { key: 'note', dataIndex: 'note', title: 'NOTE', name: 'NOTE' },
+      { key: 'start_date', dataIndex: 'start_date', title: 'START DATE', name: 'START DATE' },
+      { key: 'end_date', dataIndex: 'end_date', title: 'END DATE', name: 'END DATE' },
     ]
-    return <Table
-      rowKey={'_id'}
-      size="small"
-      bordered
-      style={{ marginTop: '5px' }}
-      columns={columns}
-      pagination={false}
-      dataSource={this.state.data_detail}
-      rowClassName={(record, index) => {
-        return index % 2 === 0 ? 'even-row' : 'old-row'
-      }}
-    />
+    return (
+      <Table
+        rowKey={'_id'}
+        size="small"
+        bordered
+        style={{ marginTop: '5px' }}
+        columns={columns}
+        pagination={false}
+        dataSource={this.state.data_detail}
+        rowClassName={(record, index) => {
+          return index % 2 === 0 ? 'even-row' : 'old-row'
+        }}
+      />
+    )
   }
 }
 
