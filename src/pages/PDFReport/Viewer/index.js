49743c727bca7c2858ff0845f302e611756237de
MinhTam <minhtamho91@gmail.com>
2018-07-16 10:23:48 +0700

    tạo form view pdf

--- src/pages/PDFReport/Viewer/index.js
+++ src/pages/PDFReport/Viewer/index.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react'
 import Page from 'components/LayoutComponents/Page'
 import CustomCard from 'components/LayoutComponents/CustomCard'
 import Helmet from 'react-helmet'
+import ViewerForm from './ViewerForm'
 
 class PdfViewer extends Component {
   static defaultProps = {
     pathName: 'Report Viewer',
-  }
-
-  componentDidMount() {
-    console.log(this.props.location.search)
   }
 
   render() {
@@ -18,7 +15,9 @@ class PdfViewer extends Component {
     return (
       <Page {...props}>
         <Helmet title="Report Viewer" />
-        <CustomCard title="Report Viewer" />
+        <CustomCard title="Report Viewer">
+            <ViewerForm dept={props.match.params.dept} />
+        </CustomCard>
       </Page>
     )
   }
