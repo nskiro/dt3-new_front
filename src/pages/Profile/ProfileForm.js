7cbcccee6dcd64034eaad5c0bcdc67e36caa7918
MinhTam <minhtamho91@gmail.com>
2018-07-24 08:59:11 +0700

    Change dept info

--- src/pages/Profile/ProfileForm.js
+++ src/pages/Profile/ProfileForm.js
@@ -31,9 +31,8 @@ const mapDispatchToProps = (dispatch, props) => ({
 )
 @Form.create()
 class ProfileForm extends Component {
-
   state = {
-    selectedDept: null
+    selectedDept: null,
   }
 
   changePassword = e => {
@@ -60,7 +59,9 @@ class ProfileForm extends Component {
           .then(res => {
             message.success('Update note complete')
             const temp = { ...this.props.user }
-            temp.dept.slice(_.findIndex(temp.dept, obj => obj._id === res.data._id), 1, {...res.data})
+            temp.dept.slice(_.findIndex(temp.dept, obj => obj._id === res.data._id), 1, {
+              ...res.data,
+            })
             this.props.updateUser(temp)
           })
           .catch(err => {
@@ -70,9 +71,8 @@ class ProfileForm extends Component {
     })
   }
   onSelectDept = value => {
-    axios.get('/api/admin/dept', {params: {id: value}})
-    .then(res => {
-      this.setState({selectedDept: res.data[0]})
+    axios.get('/api/admin/dept', { params: { id: value } }).then(res => {
+      this.setState({ selectedDept: res.data[0] })
     })
   }
 
@@ -84,82 +84,90 @@ class ProfileForm extends Component {
       <div>
         <Row>
           <Col span={5}>
-            <Select style={{width: '100%'}} onChange={this.onSelectDept}>
-              {
-                dept.length ? dept.map(obj => {
-                  return <Option key={obj._id} value={obj._id}>{obj.name}</Option>
-                }) : null
-              }
+            <Select style={{ width: '100%' }} onChange={this.onSelectDept}>
+              {dept.length
+                ? dept.map(obj => {
+                    return (
+                      <Option key={obj._id} value={obj._id}>
+                        {obj.name}
+                      </Option>
+                    )
+                  })
+                : null}
             </Select>
           </Col>
         </Row>
         <Row>
-        <Col span={5}>
-          <CustomCard title="Department Avatar">
-            <Card
-              hoverable
-              style={{ width: '90%', display: 'block', margin: 'auto' }}
-              cover={
-                selectedDept ? (
-                <img
-                  alt="example"
-                  src={`data:${selectedDept.avatar.mimetype};base64,${selectedDept.avatar.data}`}
+          <Col span={5}>
+            <CustomCard title="Department Avatar">
+              <Card
+                hoverable
+                style={{ width: '90%', display: 'block', margin: 'auto' }}
+                cover={
+                  selectedDept ? (
+                    <img
+                      alt="example"
+                      src={`data:${selectedDept.avatar.mimetype};base64,${
+                        selectedDept.avatar.data
+                      }`}
+                    />
+                  ) : null
+                }
+                actions={[
+                  <Upload
+                    name="avatar"
+                    action={`${config.baseURL}api/admin/dept/updateAvatar`}
+                    //showUploadList={false}
+                    data={selectedDept}
+                    onChange={info => {
+                      const res = info.file.response
+                      const status = info.file.status
+                      if (status === 'done') {
+                        const temp = { ...user }
+                        temp.dept = { ...res }
+                        this.props.updateUser(temp)
+                      } else if (status === 'error') {
+                        console.log(res)
+                      }
+                    }}
+                  >
+                    <Button type="primary" icon="upload">
+                      Upload New Avatar
+                    </Button>
+                  </Upload>,
+                ]}
+              >
+                <Meta
+                  title={user.fullname}
+                  description={selectedDept ? `Department: ${selectedDept.name}` : ''}
                 />
-                ) : null
-              }
-              actions={[
-                <Upload
-                  name="avatar"
-                  action={`${config.baseURL}api/admin/dept/updateAvatar`}
-                  //showUploadList={false}
-                  data={selectedDept}
-                  onChange={info => {
-                    const res = info.file.response
-                    const status = info.file.status
-                    if (status === 'done') {
-                      const temp = { ...user }
-                      temp.dept = { ...res }
-                      this.props.updateUser(temp)
-                    } else if (status === 'error') {
-                      console.log(res)
-                    }
-                  }}
-                >
-                  <Button type="primary" icon="upload">
-                    Upload New Avatar
-                  </Button>
-                </Upload>,
-              ]}
-            >
-              <Meta title={user.fullname} description={selectedDept ? `Department: ${selectedDept.name}` : ''} />
-            </Card>
-          </CustomCard>
-        </Col>
-        <Col span={11}>
-          <CustomCard title="Note">
-          {selectedDept ? (
-              <DeptInfoForm
-                ref={node => (this.deptInfoForm = node)}
-                deptInfo={selectedDept}
-                onSubmit={this.updateDeptNote}
-                key={selectedDept._id}
-              />
-            ) : null 
-          }
-          </CustomCard>
-        </Col>
-        <Col span={8}>
-          <CustomCard title="Change Account Password">
-          {selectedDept ? (
-              <PersonalInfoForm
-                ref={node => (this.personalInfoForm = node)}
-                userInfo={user}
-                onSubmit={this.changePassword}
-            /> ) : null
-          }
-          </CustomCard>
-        </Col>
-      </Row>
+              </Card>
+            </CustomCard>
+          </Col>
+          <Col span={11}>
+            <CustomCard title="Note">
+              {selectedDept ? (
+                <DeptInfoForm
+                  ref={node => (this.deptInfoForm = node)}
+                  deptInfo={selectedDept}
+                  onSubmit={this.updateDeptNote}
+                  key={selectedDept._id}
+                />
+              ) : null}
+            </CustomCard>
+          </Col>
+          <Col span={8}>
+            <CustomCard title="Change Account Password">
+              {selectedDept ? (
+                <PersonalInfoForm
+                  ref={node => (this.personalInfoForm = node)}
+                  userInfo={user}
+                  onSubmit={this.changePassword}
+                />
+              ) : null}
+            </CustomCard>
+          </Col>
+        </Row>
       </div>
     )
   }
