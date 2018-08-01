6150506628994d3d3f7f9e007aa21304cfca85c0
MinhTam <minhtamho91@gmail.com>
2018-08-01 09:28:54 +0700

    auto config port

--- src/CommonConfig.js
+++ src/CommonConfig.js
@@ -1,7 +1,7 @@
-const port = window.location.hostname === "ducthanh3.com.vn" ? 5500 : 5000
+const port = window.location.hostname === 'ducthanh3.com.vn' ? 5500 : 5000
 
 const commonConfig = {
-  baseURL: window.location.protocol + '//' + window.location.hostname + ':' + port + '/'
+  baseURL: window.location.protocol + '//' + window.location.hostname + ':' + port + '/',
 }
 
 export default commonConfig
--- src/axiosInst.js
+++ src/axiosInst.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const port = window.location.hostname === "ducthanh3.com.vn" ? 5500 : 5000
+const port = window.location.hostname === 'ducthanh3.com.vn' ? 5500 : 5000
 
 const axiosInst = axios.create({
   baseURL: window.location.protocol + '//' + window.location.hostname + ':' + port + '/',
