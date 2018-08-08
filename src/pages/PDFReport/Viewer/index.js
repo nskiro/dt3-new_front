10dae1d2212e4bb401d771503aecce8787e6a1ba
MinhTam <minhtamho91@gmail.com>
2018-08-08 10:08:12 +0700

    Update view excel

--- src/pages/PDFReport/Viewer/index.js
+++ src/pages/PDFReport/Viewer/index.js
@@ -3,6 +3,7 @@ import Page from 'components/LayoutComponents/Page'
 import CustomCard from 'components/LayoutComponents/CustomCard'
 import Helmet from 'react-helmet'
 import ViewerForm from './ViewerForm'
+import ExcelViewer from './ExcelViewer'
 
 class PdfViewer extends Component {
   static defaultProps = {
@@ -16,7 +17,9 @@ class PdfViewer extends Component {
       <Page {...props}>
         <Helmet title="Report Viewer" />
         <CustomCard title="Report Viewer">
-          <ViewerForm dept={props.match.params.dept} key={props.match.params.dept} />
+        {
+          props.match.params.dept === '5b4db4f5fd54131cdcf90c85' ? <ExcelViewer dept={props.match.params.dept} /> : <ViewerForm dept={props.match.params.dept} key={props.match.params.dept} />
+        }
         </CustomCard>
       </Page>
     )
